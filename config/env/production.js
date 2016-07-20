ba1543c53459068882549ffc3d54a2d579cabf55
trinhleo <hnkt2907@gmail.com>
2016-07-20 23:40:25 +0700

    mongolab config

--- config/env/production.js
+++ config/env/production.js
@@ -8,8 +8,7 @@ module.exports = {
   },
   port: process.env.PORT || 8443,
   db: {
-    // uri: process.env.MONGOHQ_URL || process.env.MONGOLAB_URI || 'mongodb://' + (process.env.DB_1_PORT_27017_TCP_ADDR || 'localhost') + '/vuonrau',
-    uri: 'mongodb://127.0.0.1:27017/vuonrau-dev'|| 'mongodb://admin:admin@ds011432.mlab.com:11432/vuonrau',
+    uri: process.env.MONGOHQ_URL || process.env.MONGOLAB_URI || 'mongodb://admin:admin@ds011432.mlab.com:11432/vuonrau',
     options: {
       user: '',
       pass: ''
